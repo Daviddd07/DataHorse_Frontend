93a40719cfc126438b15dec80ecb3354c1f190f1
juliann51 <julianumbarila23@gmail.com>
2026-09-26 00:40:29 -0500

    Mejorar formulario de nueva publicacion

--- src/app/Services/caballo.service.ts
+++ src/app/Services/caballo.service.ts
@@ -30,8 +30,8 @@ export interface Caballo extends NuevaPublicacion {
   providedIn: 'root',
 })
 export class CaballoService {
-  private apiUrl = 'http://localhost:8000/api/v1';
 
+  private apiUrl = 'http://localhost:8000';
   constructor(private http: HttpClient) {}
 
   listarRazas(): Observable<Raza[]> {
